1a491d119a88f346864003240f30ed63ba6bd037
Kameeeelol <seggarikamal@gmail.com>
2025-03-08 13:31:14 +0100

    news

--- Système et Réseau/tp05/barriere_non-sync.c
+++ Système et Réseau/tp05/barriere_non-sync.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 #define N 5
 #define COEFF_ATTENTE 50000
 pthread_mutex_t mutex;
-sem_t * sem; // sem_t sem pour linux 
+// sem_t *sem; // sem_t sem pour linux
+sem_t sem;
 int verif = 0;
 
 /* attendre delai microsecondes */
@@ -17,52 +18,50 @@ void work(void);
 
 void *chiffres_et_alphabet(void *);
 
-int main(void)
-{
-	int i, nums[N];
-	pthread_t th[N];
-	srand(time(NULL));
-	pthread_mutex_init(&mutex, NULL);
-	sem = sem_open("/sem", O_CREAT, 0644, 0); // sem_init(&sem,0,0) pour linux 
+int main(void) {
+  int i, nums[N];
+  pthread_t th[N];
+  srand(time(NULL));
+  pthread_mutex_init(&mutex, NULL);
+  // sem = sem_open("/sem", O_CREAT, 0644, 0); // sem_init(&sem,0,0) pour linux
+  sem_init(&sem, 0, 0);
 
-	for (i = 0; i < N; ++i) {
-		nums[i] = i;
-		if (pthread_create(&th[i], NULL, chiffres_et_alphabet, &nums[i]) != 0) {
-			perror("create");
-			return 1;
-		}
-	}
-	for (i = 0; i < N; ++i)
-		pthread_join(th[i], NULL);
-	pthread_mutex_destroy(&mutex);
-	sem_close(sem); // sem_destroy(&sem); pour linux 
-	sem_unlink("/sem");
-	return 0;
-
+  for (i = 0; i < N; ++i) {
+    nums[i] = i;
+    if (pthread_create(&th[i], NULL, chiffres_et_alphabet, &nums[i]) != 0) {
+      perror("create");
+      return 1;
+    }
+  }
+  for (i = 0; i < N; ++i)
+    pthread_join(th[i], NULL);
+  pthread_mutex_destroy(&mutex);
+  // sem_close(sem); // sem_destroy(&sem); pour linux
+  // sem_unlink("/sem");
+  sem_destroy(&sem);
+  return 0;
 }
 
-void *chiffres_et_alphabet(void *arg)
-{
-	int i = *((int *) arg);
-	printf("%d\n", i);
-	pthread_mutex_lock(&mutex);
-	verif++;
-	pthread_mutex_unlock(&mutex);
-	if (verif == N){
-		sem_post(sem); // sem_post(&sem) pour linux
-	}
-	sem_wait(sem); // sem_wait(&sem) pour linux 
-	printf("%c\n", 'A' + i);
-	sem_post(sem); // sem_post(&sem) pour linux 
+void *chiffres_et_alphabet(void *arg) {
+  int i = *((int *)arg);
+  printf("%d\n", i);
+  pthread_mutex_lock(&mutex);
+  verif++;
+  pthread_mutex_unlock(&mutex);
+  if (verif == N) {
+    sem_post(&sem); // sem_post(&sem) pour linux
+  }
+  sem_wait(&sem); // sem_wait(&sem) pour linux
+  printf("%c\n", 'A' + i);
+  sem_post(&sem); // sem_post(&sem) pour linux
 
-	return NULL;
+  return NULL;
 }
 
 void work(void) { microsleep((rand() % 10) * COEFF_ATTENTE); }
 
-void microsleep(int delai)
-{
-	struct timespec ts = { .tv_sec = delai / 1000000,
-		.tv_nsec = 1000 * (delai % 1000000)  };
-	nanosleep(&ts, NULL);
+void microsleep(int delai) {
+  struct timespec ts = {.tv_sec = delai / 1000000,
+                        .tv_nsec = 1000 * (delai % 1000000)};
+  nanosleep(&ts, NULL);
 }
--- Système et Réseau/tp05/space_invaders.c
+++ Système et Réseau/tp05/space_invaders.c
@@ -1,3 +1,5 @@
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,56 +9,85 @@
 #define NUM_ITER_DFT 30
 #define DELAI 250000
 
-int ecran[HAUTEUR][LARGEUR];
+int ecran1[HAUTEUR][LARGEUR];
+int ecran2[HAUTEUR][LARGEUR];
+int (*ecran_a_afficher)[HAUTEUR][LARGEUR] = &ecran1;
+int (*ecran_a_modifier)[HAUTEUR][LARGEUR] = &ecran2;
 void effacer_lignes(int num_lignes);
+void *calcul(void *arg);
+void *affichage(void *arg);
+void frame_prochain();
+
+sem_t sem;
+pthread_mutex_t mutex;
 
 /* Attendre delai microsecondes */
 void microsleep(int delai);
 
-int main(int argc, char *argv[])
-{
-	int num_iter = argc > 1 ? atoi(argv[1]) : NUM_ITER_DFT;
-	int n, i, j;
-	for (n = 0; n < num_iter; ++n) {
-		/* calcul de l'affichage suivant */
-		microsleep(DELAI); /* simuler un calcul long */
-		for (i = HAUTEUR - 1; i > 0; --i)
-			for (j = 0; j < LARGEUR; ++j)
-				ecran[i][j] = ecran[i - 1][j];
-		for (j = 0; j < LARGEUR; ++j)
-			ecran[0][j] = 0;
-		ecran[0][rand() % LARGEUR] = 1;
+int main(int argc, char *argv[]) {
+  int num_iter = argc > 1 ? atoi(argv[1]) : NUM_ITER_DFT;
+  sem_init(&sem, 0, 0);
+  pthread_mutex_init(&mutex, NULL);
+  pthread_t t_calcul, t_affichage;
+  pthread_create(&t_calcul, NULL, calcul, &num_iter);
+  pthread_create(&t_affichage, NULL, affichage, &num_iter);
+  pthread_join(t_calcul, NULL);
+  pthread_join(t_affichage, NULL);
+  sem_destroy(&sem);
+  pthread_mutex_destroy(&mutex);
 
-		/* affichage de l'écran modifié */
-		microsleep(DELAI); /* simuler un affichage qui prend du temps */
-		effacer_lignes(HAUTEUR);
-
-		for (i = 0; i < HAUTEUR; ++i) {
-			for (j = 0; j < LARGEUR; ++j) {
-				if (ecran[i][j] == 0)
-					putchar(' ');
-				else
-					putchar('*');
-			}
-			putchar('\n');
-		}
-	}
-	puts("YOU LOSE.\n");
-	return 0;
+  return 0;
+  puts("YOU LOSE.\n");
+  return 0;
+}
+void frame_prochain() {
+  for (int y = HAUTEUR - 1; y > 0; y--) {
+    for (int x = 0; x < LARGEUR; x++) {
+      (*ecran_a_modifier)[y][x] = (*ecran_a_afficher)[y - 1][x];
+    }
+  }
+  (*ecran_a_modifier)[0][rand() % LARGEUR] = 1;
+}
+void *calcul(void *arg) {
+  int num_iter = *(int *)arg;
+  for (int i = 0; i < num_iter; i++) {
+    frame_prochain();
+    pthread_mutex_lock(&mutex);
+    int(*temp)[HAUTEUR][LARGEUR] = ecran_a_afficher;
+    ecran_a_afficher = ecran_a_modifier;
+    ecran_a_modifier = temp;
+    pthread_mutex_unlock(&mutex);
+    sem_post(&sem);
+  }
+  return NULL;
 }
 
-void effacer_lignes(int num_lignes)
-{
-	printf("\033[2K");
-	for(; num_lignes > 0; --num_lignes) {
-		printf("\033[F");
-		printf("\033[2K");
-	}
+void *affichage(void *arg) {
+  int num_iter = *(int *)arg;
+  for (int i = 0; i < num_iter; i++) {
+    sem_wait(&sem);
+    printf("\033[2J");
+    for (int y = 0; y < HAUTEUR; y++) {
+      for (int x = 0; x < LARGEUR; x++) {
+        printf((*ecran_a_afficher)[y][x] ? "*" : " ");
+      }
+      printf("\n");
+    }
+    microsleep(10000);
+  }
+  return NULL;
 }
 
-void microsleep(int delai)
-{
-	struct timespec ts = { .tv_sec = delai / 1000000,
-		.tv_nsec = 1000 * (delai % 1000000)  };
-	nanosleep(&ts, NULL);
+void effacer_lignes(int num_lignes) {
+  printf("\033[2K");
+  for (; num_lignes > 0; --num_lignes) {
+    printf("\033[F");
+    printf("\033[2K");
+  }
+}
+
+void microsleep(int delai) {
+  struct timespec ts = {.tv_sec = delai / 1000000,
+                        .tv_nsec = 1000 * (delai % 1000000)};
+  nanosleep(&ts, NULL);
 }
